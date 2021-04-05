acb19b22a8813aafc11dc157326a4449fd344884
hssnfarooq <hhhassanhafeez91@gmail.com>
2021-04-05 20:28:52 +0500

    Rating and Reviews

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -3,13 +3,13 @@ import { Row, Col } from "react-bootstrap";
 import products from "../products";
 import Product from "../components/Product";
 
-const homeScreen = () => {
+const HomeScreen = () => {
   return (
     <>
       <h1>Latest Products</h1>
       <Row>
         {products.map((product) => (
-          <Col sm={12} md={6} lg={4} xl={3}>
+          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
             <Product product={product} />
           </Col>
         ))}
@@ -18,4 +18,4 @@ const homeScreen = () => {
   );
 };
 
-export default homeScreen;
+export default HomeScreen;
