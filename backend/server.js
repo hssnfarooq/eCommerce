efe3974fa2b238688cb739b03fe7a8474c577786
hssnfarooq <hhhassanhafeez91@gmail.com>
2021-04-14 00:26:42 +0500

    Connecting to Database(MongoDB)

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,14 @@
-const express = require("express");
-const products = require("./data/products");
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import products from "./data/products.js";
 
+dotenv.config();
+
+connectDB();
 const app = express();
 app.get("/", (req, res) => {
-  res.send("API is Running");
+  res.send("API is Running...");
 });
 
 app.get("/api/products", (req, res) => {
@@ -13,4 +18,8 @@ app.get("/api/products/:id", (req, res) => {
   const product = products.find((p) => p._id === req.params.id);
   res.json(product);
 });
-app.listen(5000, console.log("Server Running on 5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(
+  PORT,
+  console.log(`Server Running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
