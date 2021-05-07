4378329fdf88df1bfa9fdc23954279d053a4d544
hssnfarooq <hhhassanhafeez91@gmail.com>
2021-05-07 18:38:29 +0500

    Creating Custom Error Handler

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,22 @@
 import express from "express";
 import dotenv from "dotenv";
+import { notFound, errorHandler } from "./middleware/errorMiddleWare.js";
 import colors from "colors";
 import connectDB from "./config/db.js";
-import products from "./data/products.js";
+import productRoutes from "./routes/productRoutes.js";
 
 dotenv.config();
 
 connectDB();
 const app = express();
+
 app.get("/", (req, res) => {
   res.send("API is Running...");
 });
+app.use("/api/products", productRoutes);
 
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+app.use(notFound);
+app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
 app.listen(
   PORT,
